78918df0ded1fef99401271ce0d699dab1f34398
Giovanni De Toni <giovanni.det@gmail.com>
2016-07-12 08:46:36 +0200

    First working version of the preprocessor.
    It doesn't work with #include directive.

--- src/preprocessorTest.cpp
+++ src/preprocessorTest.cpp
@@ -6,14 +6,17 @@
 #include "clang/Basic/Diagnostic.h"
 #include "clang/Basic/DiagnosticOptions.h"
 #include "clang/Basic/DiagnosticIDs.h"
+#include "clang/Frontend/TextDiagnosticPrinter.h"
 #include "clang/Basic/LangOptions.h"
 #include "clang/Basic/FileManager.h"
 #include "clang/Lex/Preprocessor.h"
 #include "clang/Lex/PreprocessorOptions.h"
 #include "clang/Lex/HeaderSearch.h"
 #include "clang/Lex/HeaderSearchOptions.h"
+#include "clang/Frontend/CompilerInstance.h"
 #include "clang/Basic/LLVM.h"
 #include "clang/Basic/TargetInfo.h"
+#include <iostream>
 
 using namespace clang;
 
@@ -26,14 +29,17 @@ int main(int argc, char ** argv) {
     // Diagnostic Engine
     llvm::IntrusiveRefCntPtr<DiagnosticIDs> Diags = new DiagnosticIDs();
     DiagnosticOptions * DiagOpts = new DiagnosticOptions();
-    DiagnosticsEngine * diags = new DiagnosticsEngine(Diags, DiagOpts);
+    clang::TextDiagnosticPrinter *pTextDiagnosticPrinter = // Diagnostic Consumer
+            new clang::TextDiagnosticPrinter(llvm::outs(), DiagOpts);
+    DiagnosticsEngine * diags = new DiagnosticsEngine(Diags, DiagOpts, pTextDiagnosticPrinter);
 
     // Language options
     LangOptions * opts = new LangOptions();
 
     // Source file manager
-    FileSystemOptions optionsFilesystem;
-    SourceManager * SM = new SourceManager(*diags, *(new FileManager(optionsFilesystem)));
+    FileSystemOptions * optionsFilesystem = new FileSystemOptions();
+    FileManager * FM = new FileManager(*optionsFilesystem);
+    SourceManager * SM = new SourceManager(*diags, *FM);
 
     // Header search
     llvm::IntrusiveRefCntPtr<HeaderSearchOptions> HeaderOpts = new HeaderSearchOptions();
@@ -44,12 +50,31 @@ int main(int argc, char ** argv) {
     HeaderSearch * Headers = new HeaderSearch(HeaderOpts, *SM, *diags, *opts, target);
 
     // Module loader
-    ModuleLoader * loader;
+    CompilerInstance * loader = new CompilerInstance();
 
     // Create the preprocessor
     clang::Preprocessor * preprocessor = new clang::Preprocessor(PPOpts, *diags, *opts, *SM, *Headers, *loader);
 
 
+    // Get the file, open it and set up preprocessor
+    const FileEntry * pFile = FM->getFile(argv[1]);
+    SourceLocation * SL = new SourceLocation();
+    SM->setMainFileID(
+            SM->createFileID( pFile, *SL, clang::SrcMgr::C_User)
+    );
+    preprocessor->EnterMainSourceFile();
+
+    // Tokenize and dump tokens to stdout
+    Token * token = new Token();
+    do {
+        preprocessor->Lex(*token);
+        if(diags->hasErrorOccurred())
+        {
+            break;
+        }
+        preprocessor->DumpToken(*token);
+        std::cerr << std::endl;
+    } while(token->isNot(clang::tok::eof));
 
     return 0;
 }
