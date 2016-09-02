610b432d21df4141a05e2ffb668c1b73dafe16a7
Giovanni De Toni <giovanni.det@gmail.com>
2016-09-02 12:32:25 +0200

    Suppress IO error.

--- src/Driver.cpp
+++ src/Driver.cpp
@@ -4,28 +4,32 @@
 // This should be a better implementation of the
 // preprocessor using a CompilerInstance object.
 
-#include <clang/Basic/TargetInfo.h>
-#include "clang/Frontend/CompilerInstance.h"
-#include <clang/Frontend/TextDiagnosticPrinter.h>
-#include "clang/Lex/Preprocessor.h"
-#include "clang/Lex/HeaderSearch.h"
+#include "clang/AST/TypeLoc.h"
+#include "clang/Basic/TargetInfo.h"
 #include "clang/Driver/Driver.h"
 #include "clang/Driver/Compilation.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/TextDiagnosticPrinter.h"
 #include "clang/FrontendTool/Utils.h"
+#include "clang/Lex/Preprocessor.h"
+#include "clang/Lex/HeaderSearch.h"
 
 #include <iostream>
-#include <clang/AST/TypeLoc.h>
+#include <zconf.h>
 
 using namespace clang;
 
 int main (int argc, char ** argv) {
 
+  // Generate a ostream to evade error reporting
+  static llvm::raw_fd_ostream S(STDOUT_FILENO, false, true);
+
   CompilerInvocation * pInvocation = new CompilerInvocation();
   IntrusiveRefCntPtr<DiagnosticIDs> DiagsId (new DiagnosticIDs);
   DiagnosticOptions * DiagOpts = new DiagnosticOptions();
   clang::TextDiagnosticPrinter *DiagsCons = // Diagnostic Consumer
-          new clang::TextDiagnosticPrinter(llvm::outs(), DiagOpts);
-  std::unique_ptr<DiagnosticsEngine> Diags (new DiagnosticsEngine(DiagsId, DiagOpts, DiagsCons));
+          new clang::TextDiagnosticPrinter(S, DiagOpts);
+  DiagnosticsEngine *  Diags (new DiagnosticsEngine(DiagsId, DiagOpts, DiagsCons));
 
 
   std::vector<const char *> argDriver (argc-1);
@@ -35,14 +39,19 @@ int main (int argc, char ** argv) {
 
   clang::CompilerInvocation::CreateFromArgs(*pInvocation, &(argDriver[0]),
                                             &(argDriver[argDriver.size()]),
-                                            *(Diags.get()));
+                                            *(Diags));
 
-  std::unique_ptr<CompilerInstance> CI2 (new CompilerInstance());
+  CompilerInstance *  CI2 (new CompilerInstance());
   CI2->setInvocation(pInvocation);
-  CI2->setDiagnostics(Diags.get());
+  CI2->setDiagnostics(Diags);
 
-  clang::ExecuteCompilerInvocation(CI2.get());
-  CI2.release();
+  clang::ExecuteCompilerInvocation(CI2);
+
+  /*S.close();
+  if (S.has_error()) {
+    llvm::report_fatal_error("IO Error", false);
+
+  }*/
 
   return 0;
 }
